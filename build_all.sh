662787b149b0027534491b3dcc1911933e5158e5
Miqueas Martinez <miqueas2020@yahoo.com>
2022-02-18 18:45:30 -0500

    Added script for build all current lua versions

---
+++ build_all.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+for m in 5.{1,2,3,4}; do
+  case $m in
+    5.1 ) for p in 5.1.{0,1,2,3,4,5}; do ./build.sh $p; done ;;
+    5.2 ) for p in 5.1.{0,1,2,3,4}; do ./build.sh $p; done ;;
+    5.3 ) for p in 5.1.{0,1,2,3,4,5,6}; do ./build.sh $p; done ;;
+    5.4 ) for p in 5.1.{0,1,2,3,4}; do ./build.sh $p; done ;;
+  esac
+done
